63176cac942dcffb48ff55ed6e76e884e8ac2246
vimaan <vimaan@ttu.ee>
2017-09-27 21:16:47 +0300

    Ülesanne 1

---
+++ src/test/java/WeatherAPITests.java
@@ -0,0 +1,56 @@
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+
+public class WeatherAPITests {
+    @Test
+    public void testHttpSuccessfulConnection(){
+        //Test for response (HTML 200OK etc, we want 200).
+        fail();
+    }
+
+    @Test
+    public void receiveJSON(){
+        //Test for receiving out JSON formatted API.
+        fail();
+    }
+
+    @Test
+    public void getValidJSONData(){
+        //Test for getting valid data from JSON.
+        fail();
+    }
+
+    @Test
+    public void getWeatherToday(){
+        //Test for getting temperature for today.
+        fail();
+    }
+
+    @Test
+    public void getWeatherThreeDays(){
+        //Test for getting temperature for the next 3 days.
+        fail();
+    }
+
+    @Test
+    public void getWeatherHighestTomorrow(){
+        //Test if we can get the highest temperature for tomorrow.
+        fail();
+    }
+
+    @Test
+    public void getWeatherLowestTomorrow(){
+        //Test if we can get the lowest temperature for tomorrow.
+        fail();
+    }
+
+    @Test
+    public void receiveGeoLocation(){
+        //Test if we can get a valid Geo coordinates from a location.
+        fail();
+    }
+
+
+}
